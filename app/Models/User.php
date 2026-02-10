3ddc9cb8e61edc393c555b43e884dae3f4bbecf1
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-10 15:19:06 -0300

    chore: update User casts and property docs

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,23 +3,24 @@
 namespace App\Models;
 
 use Carbon\CarbonInterface;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 /**
  * @property-read int $id
- * @property string $name
- * @property string $email
- * @property ?CarbonInterface $email_verified_at
- * @property string $password
- * @property ?string $remember_token
- * @property ?CarbonInterface $created_at
- * @property ?CarbonInterface $updated_at
+ * @property-read string $name
+ * @property-read string $email
+ * @property-read ?CarbonInterface $email_verified_at
+ * @property-read string $password
+ * @property-read ?string $remember_token
+ * @property-read CarbonInterface $created_at
+ * @property-read CarbonInterface $updated_at
  */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -51,9 +52,12 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'id' => 'int',
+            'id' => 'integer',
+            'name' => 'string',
+            'email' => 'string',
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'remember_token' => 'string',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
         ];
