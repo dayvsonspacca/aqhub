68a17048121d7e14018e77ec2d3a1d1ada769a01
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-23 14:56:19 -0300

    feat: add Ruffle player integration and update dependencies.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,19 @@
 import axios from 'axios';
+import  '@ruffle-rs/ruffle';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+    publicPath: "/build/ruffle/",
+    backgroundColor: "#282a36",
+    quality: "high",
+    autoplay: "on",
+    unmuteOverlay: "hidden",
+    splashScreen: false,
+    showSwfDownload: true
+};
+
+import('@ruffle-rs/ruffle');
