438fb3913e4fa044633b14ee90e735e463e6d50a
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-24 16:28:35 -0300

    feat: add AqwAuthService and corresponding unit tests for token retrieval

---
+++ app/AqwSocketClient/Services/AqwAuthService.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\AqwSocketClient\Services;
+
+use Illuminate\Support\Facades\Http;
+use RuntimeException;
+
+final class AqwAuthService
+{
+    public function getToken(string $username, string $password): string
+    {
+        $response = Http::post('https://game.aq.com/game/api/login/now', [
+            'user'   => $username,
+            'pass'   => $password,
+            'option' => 1,
+        ]);
+
+        $token = $response->json('login.sToken');
+
+        throw_unless(
+            $token,
+            RuntimeException::class,
+            "Failed to retrieve account auth token for user: {$username}"
+        );
+
+        return $token;
+    }
+}
